1f878275b5910606c094027fdbd45b78635f9a96
Thomas Tu <thomasthetu@gmail.com>
2020-04-10 14:48:21 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ubc',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description='Utility Bill Calculator',
     author='Thomas Tu',
 )
